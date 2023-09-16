cb4a9dfac86b7ea7622045a08422e7b3b6097c2c
Joshua <frogman2013.jp@gmail.com>
2023-09-16 00:43:34 -0400

    Most of logic written
    addPagination function written. Still working on bug where 'active' doesnt clear from other buttons once new button has been clicked.

--- js/script.js
+++ js/script.js
@@ -8,22 +8,46 @@ function showPage(list, page) {
    let endIndex = page * 9;
    const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
-   for ( i = 0; i < list.length; i++ ) {
+   for ( i = startIndex; i < list.length; i++ ) {
       if ( i >= startIndex && i < endIndex ) {
-         const html =  `<li class="student-item cf">
+         const studentListHTML =  `<li class="student-item cf">
                            <div class="student-details">
                               <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Picture">
-                              <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
+                              <h3>${list[i].name.first} ${list[i].name.last}</h3>
                               <span class="email">${list[i].email}</span>
                            </div>
                            <div class="joined-details">
-                              <span class="date">'Joined ${list[i].registered.date}</span>
+                              <span class="date">Joined ${list[i].registered.date}</span>
                            </div>
                         </li>`;
-         studentList.insertAdjacentHTML('beforeend', html);
+         studentList.insertAdjacentHTML('beforeend', studentListHTML);
       }
    }
-}
+};
+
+function addPagination(list) {
+   const numberOfPages = Math.ceil(list.length / 9);
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
+   for ( i = 1; i < numberOfPages; i++ ) {
+      const pagesHTML = `<li><button type="button">${i}</button></li>`;
+      linkList.insertAdjacentHTML('beforeend', pagesHTML);
+   }
+   const firstButton = linkList.firstElementChild;
+   firstButton.className = 'active';
+   linkList.addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON') {
+         const button = e.target;
+         const btnValue = button.textContent;
+         const allButtons = document.querySelector('button');
+         allButtons.className = '';
+         button.className = 'active';
+         showPage(list, btnValue);
+      }
+   });
+};
 
 showPage(data, 1);
+addPagination(data);
+
 
